3016603f7bb7bc53f0ac72c1f227a86f734f11d7
Leon Wolter <leonwolter@Leons-MacBook-Pro.local>
2020-11-12 14:51:01 +0100

    fixed scraping

--- scrape_guardian.py
+++ scrape_guardian.py
@@ -15,27 +15,28 @@ class guardianSpider(scrapy.Spider):
     ]
 
 
-def parse(self, response):
-    for url in response.css("h3.fc-item__title"):
-        title = url.css("span.js-headline-text::text").extract_first(),
-        link = url.css("a::attr(href)").extract()
-        yield Request(link, callback='parse_article', meta={'title': title, 'url': link})
+    def parse(self, response):
+        for url in response.css("h3.fc-item__title"):
+            title = url.css("span.js-headline-text::text").extract_first()
+            link = url.css("a::attr(href)").extract_first()
+            yield scrapy.Request(url=link, callback=self.parse_article, meta={'title': title, 'url': link})
+
+    def parse_article(self, response):
+        yield {
+            'title': response.meta.get('title'),
+            'url': response.meta.get('url'),
+            'author': response.css("p.byline span span::text").extract_first(),
+            'publish_date': response.css("time.content__dateline-wpd::text").extract_first(),
+            'publish_time': response.css("span.content__dateline-time::text").extract_first()
+        }
 
 
-def parse_article(self, response):
-    yield {
-        'title': response.meta.get('title'),
-        'url': response.meta.get('url'),
-        'author': response.css("p.byline span span::text").extract_first(),
-        'publish_date': response.css("time.content__dateline-wpd::text").extract_first(),
-        'publish_time': response.css("span.content__dateline-time::text").extract_first()
-    }
+    def runSpider(self):
+        configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
+        runner = CrawlerRunner()
+
+        d = runner.crawl(guardianSpider)
+        d.addBoth(lambda _: reactor.stop())
+        reactor.run()  # the script will block here until the crawling is finished
 
 
-def runSpider():
-    configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
-    runner = CrawlerRunner()
-
-    d = runner.crawl(guardianSpider)
-    d.addBoth(lambda _: reactor.stop())
-    reactor.run()  # the script will block here until the crawling is finished
