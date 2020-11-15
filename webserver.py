76544f9872201a2a345c651b2ff2c4e011826060
Deverick <deverick@apollolabs.io>
2020-11-15 09:17:45 +0100

    Add periodic scanning part 2

--- webserver.py
+++ webserver.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template, request
 from flask import g
 import sqlite3 as sql
 import os
+import asyncio
+import time
+from timeloop import Timeloop
+import guardianbot
+from datetime import timedelta
+import logging
+
 app = Flask(__name__)
 
 DATABASE = r'database/dopplegaenger.db'
+tl = Timeloop()
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -43,7 +51,16 @@ def home():
     return render_template("list.html",rows = rows)
     # return render_template('home.html')
 
-if __name__ == '__main__':
-    ## Needs to be run asyn to webserver.  
+@tl.job(interval=timedelta(hours = 12))
+def run():
     os.system("python3 guardianbot.py")
-    app.run()
+
+if __name__ == '__main__':
+    tl.start(block=False)
+    while True:
+        try:
+            run()
+            app.run()
+        except KeyboardInterrupt:
+            tl.stop()
+            break
