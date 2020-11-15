da7b75b794b90dd3ab424f8ce5b4c8969583a209
Deverick <deverick@apollolabs.io>
2020-11-16 00:31:58 +0100

    Additional database changes

--- db_access.py
+++ db_access.py
@@ -22,32 +22,51 @@ def execute_sql(conn, f):
 	except Error as e:
 		logging.log(logging.ERROR, '%s raised an error', e)
 
-def create_guardian_table(conn):
-	sql_create_guardian_table = """ CREATE TABLE IF NOT EXISTS guardian (
+def create_article_table(conn):
+	sql_create_article_table = """ CREATE TABLE IF NOT EXISTS article (
                                     url text PRIMARY KEY,
                                     title text NOT NULL,
                                     author text,
-                                    publish_date text
+                                    publish_date integer
                                 ); """
-	sql_set_unique_index = """ CREATE UNIQUE INDEX url_idx ON guardian (url); """
-	execute_sql(conn, sql_create_guardian_table)
-	execute_sql(conn, sql_set_unique_index)
+	execute_sql(conn, sql_create_article_table)
 
-def insert_into_guardian(conn,item):
 
-	sql_insert_guardian_table = """INSERT INTO guardian (url, author, title, publish_date) VALUES (?, ?, ?, ?)", (item[url],item[author],item[title],item[publish_date])"""
+def create_user_table(conn):
+	sql_create_user_table = """ CREATE TABLE IF NOT EXISTS user (
+                                    user_id integer PRIMARY KEY AUTOINCREMENT,
+                                    username text NOT NULL UNIQUE
+                                ); """
+	execute_sql(conn, sql_create_user_table)
 
-	execute_sql(conn, sql_insert_guardian_table)
+def create_comment_table(conn):
+	sql_create_comment_table = """ CREATE TABLE IF NOT EXISTS comment (
+                                    message_date integer NOT NULL,
+                                    article_title text,
+                                    message_title text,
+									comment text NOT NULL,
+									user_id integer NOT NULL,
+									FOREIGN KEY (user_id)
+										REFERENCES user (user_id)
+                                ); """
+	#add a drop tables before creating table
+	#DROP TABLE suppliers;
+	execute_sql(conn, sql_create_comment_table)
+
+def insert_into_article(conn,item):
+	sql_insert_article_table = """INSERT INTO article (url, author, title, publish_date) VALUES (?, ?, ?, ?)", (item[url],item[author],item[title],item[publish_date])"""
+
+	execute_sql(conn, sql_insert_article_table)
 
 def purge_db(conn):
-	sql_purge_guardian_table = """ DROP TABLE IF EXISTS guardian;"""
-	execute_sql(conn, sql_purge_guardian_table)
+	sql_purge_article_table = """ DROP TABLE IF EXISTS article;"""
+	execute_sql(conn, sql_purge_article_table)
 
-def select_url(conn):
-	sql_select_url = """ SELECT * FROM guardian;"""
-	execute_sql(conn, sql_select_url)
+def select_article_url(conn):
+	sql_article_select_url = """ SELECT * FROM article;"""
+	execute_sql(conn, sql_article_select_url)
 
-def close_connection(conn):
+def close_db_connection(conn):
 	try:
 		conn.close()
 	except Error as e:
