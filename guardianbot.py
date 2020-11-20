dc40d12bf7f512494ce211ffa2f4fdc2cffb7473
Deverick <deverick@apollolabs.io>
2020-11-20 22:12:28 +0100

    Enable Comment Spider.  Err on URL insertion.

--- guardianbot.py
+++ guardianbot.py
@@ -16,13 +16,12 @@ from scrapy.settings import Settings
 from importlib import import_module
 
 def main():
+	#Report Log 
 	LOG_FORMAT = (
     "%(asctime)s [%(levelname)s]: %(message)s in %(pathname)s:%(lineno)d")
 	LOG_LEVEL = logging.INFO
 	# messaging logger
 	MESSAGING_LOG_FILE = os.getcwd() + "/logs/report.log"
-
-
 	messaging_logger = logging.getLogger("doppelgaenger_detection.guardianbot")
 	messaging_logger.setLevel(LOG_LEVEL)
 	messaging_logger_file_handler = FileHandler(MESSAGING_LOG_FILE)
@@ -31,16 +30,16 @@ def main():
 	messaging_logger.addHandler(messaging_logger_file_handler)
 
 
-
+	#Debug Log
 	configure_logging(install_root_handler = False)
 	logging.basicConfig(filename='logs/webapp.log', level=logging.DEBUG, format=f"%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s")
-
-
 	settings = Settings()
 	os.environ['SCRAPY_SETTINGS_MODULE'] = 'settings'
 	settings_module_path = os.environ['SCRAPY_SETTINGS_MODULE']
 	settings.setmodule(settings_module_path, priority='default')
-	runner = CrawlerRunner(settings)
+
+
+	#Database declaration and connection
 	database = r'database/dopplegaenger.db'
 	conn = create_connection(database)
 	
@@ -48,7 +47,10 @@ def main():
 		create_article_table(conn)
 		create_user_table(conn)
 		create_comment_table(conn)
-		d = runner.crawl(guardianSpider,connection=conn)
+		runner = CrawlerRunner(settings)
+		runner.crawl(guardianSpider,connection=conn)
+		runner.crawl(commentSpider,connection=conn)
+		d = runner.join() 
 		d.addBoth(lambda _: reactor.stop())
 		reactor.run()  # the script will block here until the crawling is finished
 	else:
