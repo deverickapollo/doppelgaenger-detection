04912cedf988bbc2050cc08ad769f365ec1393f4
Leon Wolter <leonwolter@Leons-MacBook-Pro.local>
2020-11-30 19:06:44 +0100

    return string instead of word list after preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,8 @@
 from nltk.corpus import stopwords
 import nltk
 import spacy
+import re
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 stop_words_dict = dict(EN=set(stopwords.words('english')),
                        ES=set(stopwords.words('spanish')),
@@ -11,13 +13,17 @@ spacy_models_dict = dict(EN="en_core_web_sm",
                          DE="de_core_news_sm",
                          FR="fr_core_news_sm")
 
-# remove stop words from a string and return a list of words
+# detokenize a list of word tokens and return a string
+def detokenize(words):
+    return TreebankWordDetokenizer().detokenize(words)
+
+# remove stop words from a string
 def remove_stop_words(string, language = "EN"):
     word_tokens = nltk.word_tokenize(string)
-    return [word for word in word_tokens if not word in stop_words_dict.get(language.upper())]
+    return detokenize([word for word in word_tokens if not word in stop_words_dict.get(language.upper())])
 
-# lemmatize string and return a list of words
+# lemmatize string
 def lemmatize(string, language = "EN"):
     nlp = spacy.load(spacy_models_dict.get(language.upper()))
     s = nlp(string)
-    return [word.lemma_ for word in s]
+    return detokenize([word.lemma_ for word in s])
