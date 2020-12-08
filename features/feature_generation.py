836e07a67bbfde50b0ddaf3b1f9cb49ff0952cef
Leon Wolter <leonwolter@Leons-MacBook-Pro.local>
2020-12-08 13:05:44 +0100

    parse config file and generate feature vector

--- features/feature_generation.py
+++ features/feature_generation.py
@@ -1,7 +1,11 @@
-import math, re, nltk, features.leetalpha as alpha, string, spacy, features.preprocessing as process
+import json
+import math, re, nltk, features.leetalpha as alpha, string, spacy, features.preprocessing as process, configparser
 #from spacy_hunspell import spaCyHunSpell
+from pprint import pprint
 from string import punctuation
 from fractions import Fraction
+from features.preprocessing import *
+
 ###################################
 ####### HELPER FUNCTIONS ##########
 ###################################
@@ -574,3 +578,149 @@ def get_language(string):
         if word in most_common_words['FR']:
             counter['FR'] += 1
     return max(counter, key=counter.get)
+
+
+def vocabulary_size(string):
+    words = nltk.word_tokenize(string)
+    voc_size = set()
+    for word in words:
+        word = word.lower()
+        if not word in punctuation:
+            voc_size.add(word)
+    return (len(voc_size), len(voc_size) / count_words(string))
+
+
+###################################
+### FEATURE VECTOR GENERATION #####
+###################################
+
+# select which string is passed to the feature generation functions based on the config file
+def select_string(cfg):
+    if cfg[1] == 0 and cfg[2] == 0:
+        return 0
+    elif cfg[1] == 1 and cfg[2] == 0:
+        return 1
+    elif cfg[1] == 0 and cfg[2] == 1:
+        return 2
+    else:
+        return 3
+
+
+# generate the feature vector based on the config file
+# returns a dict
+def feature_vector(string):
+    dict ={}
+    language = get_language(string)
+    string_remove_stop_words = remove_stop_words(string)
+    string_lemmatize = lemmatize(string, language)
+    string_remove_stop_words_lemmatize = lemmatize(string_remove_stop_words, language)
+    strings = [string, string_remove_stop_words, string_lemmatize, string_remove_stop_words_lemmatize]
+    config = configparser.ConfigParser()
+    config.readfp(open(r'feature_generation_config.cfg'))
+
+    # Character Frequency
+    cfg = json.loads(config.get("Character Frequency", "character_frequency_letters"))
+    if cfg[0] == 1:
+        dict["character_frequency_letters"] = character_frequency_letters(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Character Frequency", "character_frequency_digits"))
+    if cfg[0] == 1:
+        dict["character_frequency_digits"] = character_frequency_digits(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Character Frequency", "character_frequency_special_characters"))
+    if cfg[0] == 1:
+        dict["character_frequency_special_characters"] = character_frequency_special_characters(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Character Frequency", "character_frequency"))
+    if cfg[0] == 1:
+        dict["character_frequency"] = character_frequency(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Character Frequency", "word_length_distribution"))
+    if cfg[0] == 1:
+        dict["word_length_distribution"] = word_length_distribution(strings[select_string(cfg)])
+
+    # Vocabulary Richness
+    cfg = json.loads(config.get("Vocabulary Richness", "word_frequency"))
+    if cfg[0] == 1:
+        dict["word_frequency"] = word_frequency(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Vocabulary Richness", "number_big_words"))
+    if cfg[0] == 1:
+        dict["number_big_words"] = number_big_words(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Vocabulary Richness", "number_words_appearing_i_times"))
+    if cfg[0] == 1:
+        dict["number_words_appearing_i_times"] = number_words_appearing_i_times(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Vocabulary Richness", "yules_k"))
+    if cfg[0] == 1:
+        dict["yules_k"] = yules_k(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Vocabulary Richness", "brunets_w"))
+    if cfg[0] == 1:
+        dict["brunets_w"] = brunets_w(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Vocabulary Richness", "honores_r"))
+    if cfg[0] == 1:
+        dict["honores_r"] = honores_r(strings[select_string(cfg)])
+
+    # Sentence Level
+    cfg = json.loads(config.get("Sentence Level", "average_number_characters_sentence"))
+    if cfg[0] == 1:
+        dict["average_number_characters_sentence"] = average_number_characters_sentence(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Sentence Level", "average_number_lowercase_letters_sentence"))
+    if cfg[0] == 1:
+        dict["average_number_lowercase_letters_sentence"] = average_number_lowercase_letters_sentence(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Sentence Level", "average_number_uppercase_letters_sentence"))
+    if cfg[0] == 1:
+        dict["average_number_uppercase_letters_sentence"] = average_number_uppercase_letters_sentence(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Sentence Level", "average_number_digits_sentence"))
+    if cfg[0] == 1:
+        dict["average_number_digits_sentence"] = average_number_digits_sentence(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Sentence Level", "average_number_words_sentence"))
+    if cfg[0] == 1:
+        dict["average_number_words_sentence"] = average_number_words_sentence(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Sentence Level", "total_number_words_sentence"))
+    if cfg[0] == 1:
+        dict["total_number_words_sentence"] = total_number_words_sentence(strings[select_string(cfg)])
+
+    # Punctuation
+    cfg = json.loads(config.get("Punctuation", "punctuation_frequency"))
+    if cfg[0] == 1:
+        dict["punctuation_frequency"] = punctuation_frequency(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Punctuation", "punctuation_frequency_sentence"))
+    if cfg[0] == 1:
+        dict["punctuation_frequency_sentence"] = punctuation_frequency_sentence(strings[select_string(cfg)])
+
+    # Whitespaces
+    cfg = json.loads(config.get("Whitespaces", "repeated_whitespace"))
+    if cfg[0] == 1:
+        dict["repeated_whitespace"] = repeated_whitespace(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Whitespaces", "repeated_whitespace_sentence"))
+    if cfg[0] == 1:
+        dict["repeated_whitespace_sentence"] = repeated_whitespace_sentence(strings[select_string(cfg)])
+
+    # Idiosyncrasy
+    cfg = json.loads(config.get("Idiosyncrasy", "uppercase_words"))
+    if cfg[0] == 1:
+        dict["uppercase_words"] = uppercase_words(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Idiosyncrasy", "uppercase_words_sentence"))
+    if cfg[0] == 1:
+        dict["uppercase_words_sentence"] = uppercase_words_sentence(strings[select_string(cfg)])
+
+    # Sentiment Analysis
+    cfg = json.loads(config.get("Sentiment Analysis", "sentiment_analysis_word_average"))
+    if cfg[0] == 1:
+        dict["sentiment_analysis_word_average"] = sentiment_analysis_word_average(strings[select_string(cfg)], language)
+    cfg = json.loads(config.get("Sentiment Analysis", "sentiment_analysis_sentence_average"))
+    if cfg[0] == 1:
+        dict["sentiment_analysis_sentence_average"] = sentiment_analysis_sentence_average(strings[select_string(cfg)], language)
+
+    # Leetspeak
+    cfg = json.loads(config.get("Leetspeak", "leetspeak"))
+    if cfg[0] == 1:
+        dict["leetspeak"] = leetspeak(strings[select_string(cfg)])
+
+    # Idiosyncrasy
+    cfg = json.loads(config.get("Additional Features", "emoji_frequency_word"))
+    if cfg[0] == 1:
+        dict["emoji_frequency_word"] = emoji_frequency_word(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Additional Features", "emoji_frequency_sentence"))
+    if cfg[0] == 1:
+        dict["emoji_frequency_sentence"] = emoji_frequency_sentence(strings[select_string(cfg)])
+    cfg = json.loads(config.get("Additional Features", "get_language"))
+    if cfg[0] == 1:
+        dict["get_language"] = get_language(strings[select_string(cfg)])
+
+    return dict
