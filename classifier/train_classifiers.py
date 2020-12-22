063e326be5ef56b914b9134528be065d17e441c1
Leon Wolter <leonwolter@Leons-MacBook-Pro.local>
2020-12-22 18:31:13 +0100

    train + predict for SVC added

--- classifier/train_classifiers.py
+++ classifier/train_classifiers.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.svm import SVC
 
 
 # get the training and testing data to train a classifier for a specific user
@@ -10,3 +12,19 @@ def get_train_test_split(matrix, user_id):
     train_x, train_y = train[:, :-1], train[:, -1]
     test_x, test_y = test[:, :-1], test[:, -1]
     return (train_x, test_x, train_y, test_y)
+
+
+# train and calibrate a support vector classifier (svc)
+def train_classifier_svc(train_x, train_y):
+    model = SVC()
+    calibrated_model = CalibratedClassifierCV(model, method='sigmoid', cv=5)
+    calibrated_model.fit(train_x, train_y)
+    return calibrated_model
+
+
+# predict probabilities for a feature vector
+# input: feature vector
+# output: probabilities for each class
+def predict_probabilities_svc(clf, feature_vector):
+    predict_y = clf.predict_proba(feature_vector)
+    return predict_y
