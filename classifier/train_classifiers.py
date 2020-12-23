9a9c16a23050ad1bb01e4eddfbef4212db7d360b
Leon Wolter <leonwolter@Leons-MacBook-Pro.local>
2020-12-23 09:33:45 +0100

    model (de)serialization added

--- classifier/train_classifiers.py
+++ classifier/train_classifiers.py
@@ -1,9 +1,11 @@
 import numpy as np
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.svm import SVC
+import pickle
 
 
 # get the training and testing data to train a classifier for a specific user
+# last column of the feature matrix has to be the user id
 # input: feature matrix, user id
 # output: tuple (train_X, test_X, train_y, test_y)
 def get_train_test_split(matrix, user_id):
@@ -25,6 +27,20 @@ def train_classifier_svc(train_x, train_y):
 # predict probabilities for a feature vector
 # input: feature vector
 # output: probabilities for each class
-def predict_probabilities_svc(clf, feature_vector):
-    predict_y = clf.predict_proba(feature_vector)
+def predict_probabilities_svc(model, feature_vector):
+    predict_y = model.predict_proba(feature_vector)
     return predict_y
+
+
+# model serialization
+# innput: model
+# output: serialized model
+def save_model(model):
+    return pickle.dumps(model)
+
+
+# model deserialization
+# input: serialized model
+# output: model
+def load_model(s):
+    return pickle.loads(s)
