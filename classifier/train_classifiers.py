3a843611fa2bb17ac5c63d596e4c47ad76d31cf9
Leon Wolter <leonwolter@Leons-MacBook-Pro.local>
2020-12-23 14:17:59 +0100

    get classifiers for every user + get pairwise probability

--- classifier/train_classifiers.py
+++ classifier/train_classifiers.py
@@ -2,6 +2,22 @@ import numpy as np
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.svm import SVC
 import pickle
+import pandas as pd
+
+
+# get the classifiers for every user present in the feature matrix
+# last column of the feature matrix has to be the user id
+# input: feature matrix
+# output: dict(userid : classifier)
+def get_classifiers(matrix):
+    user_ids = set()
+    classifiers = dict()
+    for list in matrix:
+        user_ids.add(list[-1])
+    for user_id in user_ids:
+        train_x, test_x, train_y, test_y = get_train_test_split(matrix, user_id)
+        classifiers[user_id] = train_classifier_svc(train_x, train_y)
+    return classifiers
 
 
 # get the training and testing data to train a classifier for a specific user
@@ -25,11 +41,28 @@ def train_classifier_svc(train_x, train_y):
 
 
 # predict probabilities for a feature vector
-# input: feature vector
+# input: classifier model, feature matrix
 # output: probabilities for each class
-def predict_probabilities_svc(model, feature_vector):
-    predict_y = model.predict_proba(feature_vector)
+def predict_probabilities_svc(model, feature_matrix):
+    predict_y = pd.DataFrame(model.predict_proba(feature_matrix), columns=model.classes_)
     return predict_y
+
+
+# predict the pairwise probability for two feature vectors in a matrix
+# input: classifier models, feature matrix with two feature vectors
+# output: pairwise probability dict(average, multiplication, squared_average)
+def predict_pairwise_probability_svc(models, feature_matrix):
+    predictions = []
+    i=1
+    for line in feature_matrix:
+        p = predict_probabilities_svc(models[line[-1]], [line[:-1]])
+        predictions.append(p.get(feature_matrix[i%2][-1])[0])
+        i += 1
+    print(predictions)
+    pairwise_prob = dict(average = np.average(predictions),
+                         multiplication = np.prod(predictions),
+                         squared_average = np.average(np.square(predictions)))
+    return pairwise_prob
 
 
 # model serialization
